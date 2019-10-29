350c6a4e0afe77446013eced1207020b8fc9653c
Mark <dhkdn4042@naver.com>
2019-10-29 11:45:17 +0900

    Scheduler 추가 및 불필요한 코드 제거

--- src/main/java/com/essri/webtoon/WebtoonApplication.java
+++ src/main/java/com/essri/webtoon/WebtoonApplication.java
@@ -1,11 +1,12 @@
 package com.essri.webtoon;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableJpaAuditing
 public class WebtoonApplication {
 
--- src/main/java/com/essri/webtoon/web/controller/WebRestController.java
+++ src/main/java/com/essri/webtoon/web/controller/WebRestController.java
@@ -27,27 +27,6 @@ public class WebRestController {
     private final Environment env;
 
     /**
-     * Batch 작업으로 바꿀 메소드(웹툰 목록 파싱)
-     * @return insert 된 데이터 @Batch 돌리는 형태로 바꿀거임
-     */
-    @RequestMapping(method = RequestMethod.POST, value="/insertData")
-    @ResponseStatus(value = HttpStatus.CREATED)
-    public List<Toons> crawlData() {
-//        toonservice.crawlDaumData();
-        return toonservice.crawlNaverData();
-    }
-
-    /**
-     * 네이버 웹툰 리스트 불러오기
-     * @return toons 테이블의 모든 행 @TEST용
-     */
-    @RequestMapping(method = RequestMethod.GET, value="/getList")
-    @ResponseStatus(value = HttpStatus.OK)
-    public List<ToonsDTO.ListRes> getList() {
-        return toonservice.getList();
-    }
-
-    /**
      * 웹툰 리스트 불러오기
      * @return toons 테이블 (entity: ConvertWebToonLists)
      */
@@ -56,7 +35,6 @@ public class WebRestController {
     public Map<String, Map<String, List<ToonsDTO.ConvertWebToonLists>>> getConvertList() {
         return toonservice.convertList();
     }
-
 
     /**
      * TODO 회원가입 기능 작성중..
