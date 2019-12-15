eb3c3bc02a77dbd89937682890deca7c883417dd
Mark <dhkdn4042@naver.com>
2019-12-15 23:31:08 +0900

    request token filter 추가

---
+++ src/main/java/com/essri/webtoon/filter/RequestFilter.java
@@ -0,0 +1,41 @@
+package com.essri.webtoon.filter;
+
+import com.essri.webtoon.exception.BusinessException;
+import com.essri.webtoon.exception.ErrorCode;
+import com.essri.webtoon.web.dto.BaseRestResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Enumeration;
+
+import static com.essri.webtoon.exception.ErrorCode.HANDLE_ACCESS_DENIED;
+
+@Slf4j
+@Component
+public class RequestFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        Enumeration<String> headerNames = req.getHeaderNames();
+
+
+        if (headerNames != null) {
+            while (headerNames.hasMoreElements()) {
+                String name = headerNames.nextElement();
+                if(name.equals("token")) {
+                    log.info("token is {}", req.getHeader(name));
+                    // TODO req token 이 유효한지 검사
+                }
+            }
+        } else {
+            throw new BusinessException(HANDLE_ACCESS_DENIED);
+        }
+
+        chain.doFilter(request, response);
+
+    }
+}
