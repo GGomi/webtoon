599f058c1aa19f5ba73b846d37a6ae7173cf095f
Mark <dhkdn4042@naver.com>
2019-12-30 20:39:50 +0900

    filter 임시제거

--- src/main/java/com/essri/webtoon/filter/RequestFilter.java
+++ src/main/java/com/essri/webtoon/filter/RequestFilter.java
@@ -13,26 +13,26 @@ import static com.essri.webtoon.exception.ErrorCode.HANDLE_ACCESS_DENIED;
 
 @Slf4j
 @Component
-public class RequestFilter implements Filter {
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
-        HttpServletRequest req = (HttpServletRequest) request;
-        Enumeration<String> headerNames = req.getHeaderNames();
-
-        if (headerNames != null) {
-            while (headerNames.hasMoreElements()) {
-                String name = headerNames.nextElement();
-                if(name.equals("token")) {
-                    log.info("token is {}", req.getHeader(name));
-                    // TODO req token 이 유효한지 검사
-                }
-            }
-        } else {
-            throw new BusinessException(HANDLE_ACCESS_DENIED);
-        }
-
-        chain.doFilter(request, response);
-
-    }
+public class RequestFilter {
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+//
+////        HttpServletRequest req = (HttpServletRequest) request;
+////        Enumeration<String> headerNames = req.getHeaderNames();
+////
+////        if (headerNames != null) {
+////            while (headerNames.hasMoreElements()) {
+////                String name = headerNames.nextElement();
+////                if(name.equals("token")) {
+////                    log.info("token is {}", req.getHeader(name));
+////                    // TODO req token 이 유효한지 검사
+////                }
+////            }
+////        } else {
+////            throw new BusinessException(HANDLE_ACCESS_DENIED);
+////        }
+////
+////        chain.doFilter(request, response);
+//
+//    }
 }
